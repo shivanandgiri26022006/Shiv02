9ba26c148bde5c17cd01f55f91febfbcd0231f89
Shivanand Giri <shivanandgiri026@gmail.com>
2025-11-12 11:07:03 +0530

    Add files via upload

---
+++ fp2.c
@@ -0,0 +1,18 @@
+\\while loop
+#include<stdio.h>
+
+int main (){
+    int n;
+    printf("Entr your your no :");
+    scanf("%d",&n);
+
+  int  i = 0;
+    while ( i<=n){
+printf("%d\n",i);
+i++;
+
+    }
+
+    return 0;
+    
+}
